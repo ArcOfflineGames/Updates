31296c2b300d61997ce7b6aea59c49cfe6dea48f
ArcOfflineGames <50846615+ArcOfflineGames@users.noreply.github.com>
2019-05-23 08:48:18 -0500

    Update update.js

--- update.js
+++ update.js
@@ -1,2 +1,2 @@
-var newVersion = [1,7,0];
+var newVersion = [1,6,0];
 var added = ["Added update checker","Added Castlevania","Added Tank Trouble","Added Flappy Bird","Added Donkey Kong", "Added Joust", "Added Galaga"];
